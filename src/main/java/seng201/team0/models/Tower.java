9b8eba8e7d702aee67b4c10ac1deecd72e5e8529
mam417 <mam417@uclive.ac.nz>
2024-04-25 19:33:26 +1200

    Edited the documentation for Tower class, and defaulting the cost in constructor. Added the GameEnvironment class containing methods for launching the setup screen, main screen, and clear screen functionality. Documentation is also included.

--- src/main/java/seng201/team0/models/Tower.java
+++ src/main/java/seng201/team0/models/Tower.java
@@ -3,87 +3,84 @@ package seng201.team0.models;
 
 /**
  * Tower class containing default stats such as resource amount,
- * reloading speed, resource type, level, and cost
- * @author Allen - Team 75
- *
+ * reloading speed, resource type, level, and cost.
  */
 public class Tower {
-    private String name;
-    private String resourceType;
-    private int resourceAmount;
-    private int reloadSpeed;
-    private int level;
-    private int cost;
+    private String name; // Name of tower.
+    private String resourceType; // Resource type of tower.
+    private int resourceAmount; // Resource amount of tower.
+    private int reloadSpeed; // Reload speed of tower.
+    private int level; // Level of tower.
+    private int cost; // Cost of tower.
 
     /**
-     *Constructor for Tower
-     * @param name name of tower
-     * @param resourceType resource type of tower
-     * @param cost cost of tower
-     * Initializes the value for resource amount, reload speed, and level of tower
+     *Constructor for Tower.
+     * @param name A String name of tower
+     * @param resourceType An int resource type of tower
+     * Initializes the value for resource amount, reload speed, level of tower, and cost (all int type).
      */
-    public Tower(String name, String resourceType, int cost){
+    public Tower(String name, String resourceType){
         this.name = name;
         this.resourceType = resourceType;
         this.resourceAmount = 500;
         this.reloadSpeed = 25;
         this.level = 1;
-        this.cost = cost;
+        this.cost = 60;
     }
 
     /**
-     * Method for getting the name of the tower
-     * @return name of tower
+     * Method for getting the name of the tower.
+     * @return A string representing the name of tower.
      */
     public String getName(){
         return name;
     }
 
     /**
-     * Sets the new name of tower
-     * @param name new name of tower
+     * Sets the name of tower.
+     * @param name A string representing the new name of tower.
      */
     public void setName(String name){
         this.name = name;
     }
 
     /**
-     * Method for getting the amount of resources of tower
-     * @return resource amount of tower
+     * Method for getting the amount of resources of tower.
+     * @return An int representing the resource amount of tower.
      */
     public int getResourceAmount(){
         return resourceAmount;
     }
 
     /**
-     * Method for getting the amount of reload speed
-     * @return reload speed of tower
+     * Method for getting the amount of reload speed.
+     * @return An int representing the reload speed of tower.
      */
     public int getReloadSpeed(){
         return reloadSpeed;
     }
 
     /**
-     * Method for getting the resource type of tower
-     * @return resource type of tower
+     * Method for getting the resource type of tower.
+     * @return A string representing the resource type of tower.
      */
     public String getResourceType(){
         return resourceType;
     }
 
     /**
-     * Sets the resource type of the tower
-     * @param resourceType resource type of tower
+     * Sets the resource type of the tower.
+     * @param resourceType A String representing the new resource type of tower.
      */
     public void setResourceType(String resourceType){
         this.resourceType = resourceType;
     }
 
     /**
-     * Method for upgrading the level of tower
-     * Increases the level of tower by 1
-     * Increases the resource amount by 500
-     * Increases the reload speed by 25
+     * Method for upgrading the level of tower.
+     * Increases the level of tower by 1.
+     * Increases the resource amount by 500.
+     * Increases the reload speed by 25.
      */
     public void levelUp(){
         level++;
